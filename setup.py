d1614bed12cde428b78093a98a58b6b9ec2b99fb
Devindudh <3ddevindu@gmail.com>
2025-08-14 13:37:30 +0530

    updated-setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="datapeek",
-    version="0.1.0",
-    author="Your Name",
+    version="0.2.0",  # bumped version since this is a feature upgrade
+    author="DevinduDh",
     author_email="youremail@example.com",
-    description="A quick glance at your DataFrame.",
+    description="A quick glance at your DataFrame with beautiful terminal and notebook output.",
     packages=find_packages(),
-    install_requires=["pandas", "tabulate"],
+    install_requires=[
+        "pandas",
+        "tabulate",
+        "rich"
+    ],
     python_requires=">=3.7",
 )
